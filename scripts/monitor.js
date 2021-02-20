372b671a7df2ab9701ac5682563721276a0d03a3
Ole Petter <ole.orhagen@northern.tech>
2021-02-20 09:42:31 +0100

    Update monitor.js
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- scripts/monitor.js
+++ scripts/monitor.js
@@ -1,15 +1,37 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const url = "https://debian.beagleboard.org/images"
-const imageName = "bone-debian-10.3-iot-armhf-2020-04-06-4gb.img.xz"
+const fs = require('fs')
+
+// How to update the given file after a new version is found (?)
+
+// const url = "https://debian.beagleboard.org/images"
+// const imageName = "bone-debian-10.3-iot-armhf-2020-04-06-4gb.img.xz"
 var reg = "bone-debian-(?<version>[0-9]+\.[0-9]+)-iot-armhf-(?<date>[0-9]{4}-[0-9]{2}-[0-9]{1,2})-4gb.img.xz$"
+
+// Read the input file, and parse the variable input
+try {
+    const data = fs.readFileSync('test/run-tests.sh', 'utf8')
+          .split('\n')
+          .filter(line => line.match("BBB_DEBIAN_SDCARD_IMAGE_URL=.*"))
+    var line = data[0]
+    console.log(line)
+    var m = line.match(".*=\"(?<url>[a-zA-Z-://\._]*)(?<imageName>bone-debian-(?<version>[0-9]+\.[0-9]+)-iot-armhf-(?<date>[0-9]{4}-[0-9]{2}-[0-9]{1,2})-4gb.img.xz)")
+    console.log(m)
+    var url = m.groups.url
+    var imageName = m.groups.imageName
+    var version = m.groups.version
+    var date = m.groups.date
+} catch (err) {
+    console.error(err)
+    process.exit(1)
+}
 
 // The bone-debian setup has two parts which needs comparing:
 // * The release-version: i.e., 10.3
 // * The date: i.e., 2020-04-06
 
-JSDOM.fromURL("https://debian.beagleboard.org/images", {}).then(dom => {
+JSDOM.fromURL(url, {}).then(dom => {
     var document = dom.window.document;
     var table = document.getElementById("list");
     var rows = table.rows;
@@ -36,7 +58,25 @@ JSDOM.fromURL("https://debian.beagleboard.org/images", {}).then(dom => {
         }
         return parseFloat(b.version) - parseFloat(a.version);
     });
+    console.log(matches[0].text)
     if (matches[0].text !== imageName) {
         console.log("We've got a new release! \\o/");
+        console.log("BBB_DEBIAN_SDCARD_IMAGE_URL=${url}/${matches[0].text}")
+        updateURLLink("BBB_DEBIAN_SDCARD_IMAGE_URL=${url}/${matches[0].text}")
     }
 });
+
+function updateURLLink(newLine) {
+    console.error("Updating the variable")
+    try {
+        const data = fs.readFileSync('test/run-tests.sh', 'utf8').replace(/## Auto-update\nBBB_DEBIAN_SDCARD_IMAGE_URL=.*/, `## Auto-update\n${newLine}\n`)
+        fs.writeFile('test/run-tests.sh', data, (err, data) => {
+            if (err) {
+                console.error(err)
+            }
+        })
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
